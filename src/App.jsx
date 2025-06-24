c2d0e8120ac3a95bf76b8d949ef1002f4c4a8f1e
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2025-06-24 07:18:19 -0500

    rename header to GeneralInfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import {GeneralInfo} from "./components/GeneralInfo";
+import {Education} from "./components/Education";
+import {Expericence} from "./components/Experience";
 
 import './App.css'
 
 function App() {
   return (
     <>
-
+    <div>
+      <GeneralInfo></GeneralInfo>
+    </div>
+    <div>
+      <Education></Education>
+    </div>
+    <div>
+      <Experience></Experience>
+    </div>
     </>
   )
 }
---
+++ src/components/GeneralInfo.jsx
@@ -0,0 +1,11 @@
+import {GeneralInfoForm} from "./GeneralInfoForm.jxs";
+
+export default function GeneralInfo(){
+    return(
+        <div id="general-info-container">
+            <H1>General Information</H1>
+            <p>Please fill out the information below</p>
+            
+        </div>
+    );
+}
