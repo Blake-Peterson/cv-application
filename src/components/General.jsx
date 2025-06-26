fa439cb315edba8aecedc4758e43c1344182b5fc
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2025-06-25 20:41:49 -0500

    General.jsx parses information and displays on form

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -4,9 +4,12 @@ export default function General(){
 
     const [inputs, setInputs]=useState("");
 
+    const [isActive,setIsActive] = useState(false);
+
     const handleSubmit = (event) =>{
         event.preventDefault();
         console.log("form submitted");
+        setIsActive(true);
     }
 
     const handleChange = (event) =>{
@@ -14,58 +17,66 @@ export default function General(){
         const value = event.target.value;
         setInputs(values => ({...values, [name]:value}) );
     }
-            
 
     return(
         <div id="general-info-container">
             <h1>General Information</h1>
             <p>Please fill out the information below</p>
-
             <div id="general-info-form-container">
-                <form action="./GeneralInfo.jsx" method="get" onSubmit={handleSubmit}>
-                    <div className="form-contents">
-                        <label htmlFor="fname">First Name:</label>
-                        <input 
-                            type="text" 
-                            id="fname" 
-                            name="fname"
-                            value = {inputs.fname}
-                            onChange={handleChange}
-                        />
+                {!isActive ? (
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-contents">
+                            <label htmlFor="fname">First Name:</label>
+                            <input 
+                                type="text" 
+                                id="fname" 
+                                name="fname"
+                                value = {inputs.fname}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-contents">
+                            <label htmlFor="lname">Last Name:</label>
+                            <input 
+                                type="text"     
+                                id="lname" 
+                                name="lname"
+                                value = {inputs.lname}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-contents">
+                            <label htmlFor="email">Email Address:</label>
+                            <input 
+                                type="email" 
+                                id="email" 
+                                name="email"
+                                value = {inputs.email}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-contents">
+                            <label htmlFor="phone">Phone Number:</label>
+                            <input 
+                                type="tel" 
+                                id="phone" 
+                                name="phone"
+                                value = {inputs.phone}
+                                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <input type="submit" />
+                    </form>
+                ) : (
+                    <div className="form-data">
+                        <p>First Name: {inputs.fname}</p>
+                        <p>Last Name: {inputs.lname}</p>
+                        <p>Email: {inputs.email}</p>
+                        <p>Phone: {inputs.phone}</p>
+                        <button onClick={() => setIsActive(false)}>Edit</button>
                     </div>
-                    <div className="form-contents">
-                        <label htmlFor="lname">Last Name:</label>
-                        <input 
-                            type="text"     
-                            id="lname" 
-                            name="lname"
-                            value = {inputs.lname}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="form-contents">
-                        <label htmlFor="email">Email Address:</label>
-                        <input 
-                            type="email" 
-                            id="email" 
-                            name="email"
-                            value = {inputs.email}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="form-contents">
-                        <label htmlFor="phone">Phone Number:</label>
-                        <input 
-                            type="tel" 
-                            id="phone" 
-                            name="phone"
-                            value = {inputs.cellphone}
-                            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <input type="submit" />
-                </form>
+                )}        
             </div>
         </div>
     );
