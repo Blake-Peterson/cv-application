de656163e7e81ff3361d31e60df2d6f7a2da5c6a
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2025-06-25 20:52:19 -0500

    updated Education and Experience to update forms on submits

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,10 +3,12 @@ import {useState} from 'react';
 export default function Education(){
 
     const [inputs, setInputs]=useState("");
+    const [isActive, setIsActive]=useState(false);
 
     const handleSubmit = (event) =>{
         event.preventDefault();
         console.log("form submitted");
+        setIsActive(true);
     }
 
     const handleChange = (event) =>{
@@ -20,49 +22,59 @@ export default function Education(){
             <h1>Education</h1>
             <p>Please fill out the information below</p>
             <div id="education-form-container">
-                <form action="./App.jsx" method="get" onSubmit={handleSubmit}>
-                    <div className="form-contents">
-                        <label htmlFor="school">Name of School:</label>
-                        <input 
-                            type="text" 
-                            id="school" 
-                            name="school"
-                            value = {inputs.school}
-                            onChange={handleChange}
-                        />
+                { !isActive ? (                
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-contents">
+                            <label htmlFor="school">Name of School:</label>
+                            <input 
+                                type="text" 
+                                id="school" 
+                                name="school"
+                                value = {inputs.school}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-contents">
+                            <label htmlFor="major">Major:</label>
+                            <input 
+                                type="text"     
+                                id="major" 
+                                name="major"
+                                value = {inputs.major}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-contents">
+                            <label htmlFor="school_start">School Start Date:</label>
+                            <input 
+                                type="date" 
+                                id="school_start" 
+                                name="school_start"
+                                value = {inputs.school_start}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-contents">
+                            <label htmlFor="school_end">School End Date:</label>
+                            <input 
+                                type="date" 
+                                id="school_end" 
+                                name="school_end"
+                                value = {inputs.school_end}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <input type="submit" />
+                    </form>
+                ) : (
+                        <div className="form-data">
+                        <p>School: {inputs.school}</p>
+                        <p>Major: {inputs.major}</p>
+                        <p>Start Date: {inputs.school_start}</p>
+                        <p>End Date: {inputs.school_end}</p>
+                        <button onClick={() => setIsActive(false)}>Edit</button>
                     </div>
-                    <div className="form-contents">
-                        <label htmlFor="major">Major:</label>
-                        <input 
-                            type="text"     
-                            id="major" 
-                            name="major"
-                            value = {inputs.major}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="form-contents">
-                        <label htmlFor="school_start">School Start Date:</label>
-                        <input 
-                            type="date" 
-                            id="school_start" 
-                            name="school_start"
-                            value = {inputs.school_start}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="form-contents">
-                        <label htmlFor="school_end">School End Date:</label>
-                        <input 
-                            type="date" 
-                            id="school_end" 
-                            name="school_end"
-                            value = {inputs.school_end}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <input type="submit" />
-                </form>
+                )}
             </div>
         </div>
     );
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,10 +3,12 @@ import {useState} from 'react';
 export default function Experience(){
 
     const [inputs, setInputs]=useState("");
+    const [isActive, setIsActive] = useState(false);
 
     const handleSubmit = (event) =>{
         event.preventDefault();
         console.log("form submitted");
+        setIsActive(true);
     }
 
     const handleChange = (event) =>{
@@ -20,59 +22,71 @@ export default function Experience(){
             <h1>Expericence</h1>
             <p>Please fill out the information below</p>
             <div id="experience-form-container">
-                <form action="./App.jsx" method="get" onSubmit={handleSubmit}>
-                    <div className="form-contents">
-                        <label htmlFor="company_name">Company Name:</label>
-                        <input 
-                            type="text" 
-                            id="company_name" 
-                            name="company_name"
-                            value = {inputs.company_name}
-                            onChange={handleChange}
-                        />
+                {!isActive ? (
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-contents">
+                            <label htmlFor="company_name">Company Name:</label>
+                            <input 
+                                type="text" 
+                                id="company_name" 
+                                name="company_name"
+                                value = {inputs.company_name}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-contents">
+                            <label htmlFor="job_title">Job Title</label>
+                            <input 
+                                type="text"     
+                                id="job_title" 
+                                name="job_title"
+                                value = {inputs.job_title}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-contents">
+                            <label htmlFor="description">Job Description</label>
+                            <input 
+                                type="text"     
+                                id="description" 
+                                name="description"
+                                value = {inputs.description}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-contents">
+                            <label htmlFor="job_start_date">Start Date:</label>
+                            <input 
+                                type="date" 
+                                id="job_start_date" 
+                                name="job_start_date"
+                                value = {inputs.job_start_date}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-contents">
+                            <label htmlFor="job_end_date">End Date:</label>
+                            <input 
+                                type="date" 
+                                id="job_end_date" 
+                                name="job_end_date"
+                                value = {inputs.job_end_date}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <input type="submit" />
+                    </form>
+                ): (
+                    <div className="form-data">
+                        <p>Company Name: {inputs.company_name}</p>
+                        <p>Title: {inputs.job_title}</p>
+                        <p>Description: {inputs.description}</p>
+                        <p>Start Date: {inputs.job_start_date}</p>
+                        <p>End Date: {inputs.job_end_date}</p>
+                        <p></p>
+                        <button onClick={() => setIsActive(false)}>Edit</button>
                     </div>
-                    <div className="form-contents">
-                        <label htmlFor="job_title">Job Title</label>
-                        <input 
-                            type="text"     
-                            id="job_title" 
-                            name="job_title"
-                            value = {inputs.job_title}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="form-contents">
-                        <label htmlFor="description">Job Description</label>
-                        <input 
-                            type="text"     
-                            id="description" 
-                            name="description"
-                            value = {inputs.description}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="form-contents">
-                        <label htmlFor="job_start_date">Start Date:</label>
-                        <input 
-                            type="date" 
-                            id="job_start_date" 
-                            name="job_start_date"
-                            value = {inputs.job_start_date}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="form-contents">
-                        <label htmlFor="job_end_date">End Date:</label>
-                        <input 
-                            type="date" 
-                            id="job_end_date" 
-                            name="job_end_date"
-                            value = {inputs.job_end_date}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <input type="submit" />
-                </form>
+                )}
             </div>
         </div>
     );
